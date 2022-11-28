1517959dba8d528d72fd6d31c2a86f55147b6474
kranthigy <kranthi@goodyear.com>
2022-11-28 06:56:31 -0500

    sum_of_two

---
+++ python_basics.py
@@ -0,0 +1,19 @@
+# 0:00:00 Introduction
+# 0:00:30 What You Can Do With Python
+# 0:01:15 Your First Python Program
+# 0:05:30 Variables
+# 0:09:08 Receiving Input
+# 0:10:48 Type Conversion
+# 0:18:49 Strings
+# 0:23:41 Arithmetic Operators
+# 0:25:59 Operator Precedence
+# 0:27:11 Comparison Operators
+# 0:28:52 Logical Operators
+# 0:31:06 If Statements
+# 0:36:16 Exercise
+# 0:41:42 While Loops
+# 0:45:11 Lists
+# 0:48:47 List Methods
+# 0:52:16 For Loops
+# 0:54:54 The range() Function
+# 0:57:43 Tuples
